2d2add02541b28ad730b796e3a3a7e4a83bc11e6
Disaylt <disofban@gmail.com>
2022-07-14 13:49:20 +0300

    create ProjectSettings

---
+++ ZpSolutionWbBuyoutsParser/ProjectConfig.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZennoLab.InterfacesLibrary.ProjectModel;
+
+namespace ZpSolutionWbBuyoutsParser
+{
+    internal class ProjectConfig
+    {
+        private static ProjectConfig _projectConfig;
+        private string _projectPath { get; set; }
+        private ProjectConfig() { }
+        private ProjectConfig(string projectPath)
+        {
+            _projectPath = projectPath;
+        }
+
+        public void Initialize(IZennoPosterProjectModel project)
+        {
+            if(_projectConfig == null)
+            {
+                _projectConfig = new ProjectConfig(project.Directory);
+            }
+        }
+
+        public ProjectConfig GetInstance()
+        {
+            if(_projectConfig != null)
+            {
+                return _projectConfig;
+            }
+            else
+            {
+                throw new NullReferenceException("ProjectConfig is not loaded");
+            }
+        }
+    }
+}
