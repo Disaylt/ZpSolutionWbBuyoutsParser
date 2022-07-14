550e79bf9eb5b8ae8074623773fb76de2d111725
Disaylt <disofban@gmail.com>
2022-07-14 13:56:33 +0300

    create json loader

---
+++ ZpSolutionWbBuyoutsParser/JsonLoader.cs
@@ -0,0 +1,27 @@
+﻿using Global.ZennoLab.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZpSolutionWbBuyoutsParser
+{
+    internal class JsonLoader
+    {
+        internal static T LoadJson<T>(string filePath)
+        {
+            try
+            {
+                string content = File.ReadAllText(filePath);
+                T jsonObject = JToken.Parse(content).ToObject<T>();
+                return jsonObject;
+            }
+            catch
+            {
+                return default;
+            }
+        }
+    }
+}
