53a13db80222a703333d668fb083146a749fc927
Disaylt <disofban@gmail.com>
2022-07-14 13:40:32 +0300

    create setting model

---
+++ ZpSolutionWbBuyoutsParser/Models/Json/ProjectSettingsModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZpSolutionWbBuyoutsParser.Models.Json
+{
+    internal class ProjectSettingsModel
+    {
+        public DateTime LastWorkDate { get; set; }
+        public int AmountOfDaysForParsing { get; set; } = 7;
+        public bool UseDaysFilter { get; set; } = true;
+
+    }
+}
--- ZpSolutionWbBuyoutsParser/Program.cs
+++ ZpSolutionWbBuyoutsParser/Program.cs
@@ -24,7 +24,7 @@ namespace ZpSolutionWbBuyoutsParser
         /// </summary>
         /// <param name="instance">Объект инстанса выделеный для данного скрипта</param>
         /// <param name="project">Объект проекта выделеный для данного скрипта</param>
-        /// <returns>Код выполнения скрипта</returns>		
+        /// <returns>Код выполнения скрипта</returns>
         public int Execute(Instance instance, IZennoPosterProjectModel project)
         {
             int executionResult = 0;
