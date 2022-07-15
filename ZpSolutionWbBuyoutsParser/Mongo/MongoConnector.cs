86894ecc5f72d7e87a96f9b8b2b729242c699ff0
Disaylt <disofban@gmail.com>
2022-07-15 11:10:32 +0300

    implementation mongo connector

--- ZpSolutionWbBuyoutsParser/Mongo/MongoConnector.cs
+++ ZpSolutionWbBuyoutsParser/Mongo/MongoConnector.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MongoDB.Driver;
 
 namespace ZpSolutionWbBuyoutsParser.Mongo
 {
     internal abstract class MongoConnector
     {
+        private static MongoClient _client;
 
+        public MongoConnector()
+        {
+            if( _client == null )
+            {
+                _client = CreateClient();
+            }    
+        }
+
+        public IMongoDatabase GetDatabase(string name)
+        {
+            IMongoDatabase database = _client.GetDatabase(name);
+            return database;
+        }
+
+        private MongoClient CreateClient()
+        {
+            MongoSettings settings = new MongoSettings();
+            string connectString = settings.GetSettings().ConnectionString;
+            MongoClient client = new MongoClient( connectString );
+            return client;
+        }
     }
 }
