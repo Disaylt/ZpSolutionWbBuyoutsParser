1a5f1d6e1a40039bea1c30581265c3793f847570
Disaylt <disofban@gmail.com>
2022-07-15 11:21:05 +0300

    create wb buyouts database

---
+++ ZpSolutionWbBuyoutsParser/Mongo/WbBuyoutsDatabase.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+
+namespace ZpSolutionWbBuyoutsParser.Mongo
+{
+    internal class WbBuyoutsDatabase : MongoConnector
+    {
+        protected IMongoDatabase DataBase
+        {
+            get
+            {
+                return _database;
+            }
+        }
+
+        private static IMongoDatabase _database;
+        private const string _dbName = "wb_buyouts";
+
+        public WbBuyoutsDatabase()
+        {
+            if(_database == null)
+            {
+                _database = GetDatabase(_dbName);
+            }    
+        }
+    }
+}
