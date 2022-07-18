3026723558afc2ef1b88c5b9d85136659fbe77d5
Disaylt <disofban@gmail.com>
2022-07-18 15:03:48 +0300

    create planning objects

---
+++ ZpSolutionWbBuyoutsParser/Models/Bson/WB/PlanningModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ZpSolutionWbBuyoutsParser.Models.Bson.WB
+{
+    internal class PlanningModel
+    {
+        [BsonId]
+        public BsonObjectId Id { get; set; }
+
+        [BsonElement("date")]
+        public DateTime CreateDate { get; set; }
+
+        [BsonElement("session")]
+        public string Session { get; set; }
+    }
+}
--- ZpSolutionWbBuyoutsParser/Mongo/WbPlanningCollection.cs
+++ ZpSolutionWbBuyoutsParser/Mongo/WbPlanningCollection.cs
@@ -1,12 +1,39 @@
-﻿using System;
+﻿using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ZpSolutionWbBuyoutsParser.Models.Bson.WB;
 
 namespace ZpSolutionWbBuyoutsParser.Mongo
 {
     internal class WbPlanningCollection : WbBuyoutsDatabase
     {
+        private const string _name = "planning";
+
+        private static IMongoCollection<PlanningModel> _mongoCollection;
+
+        public WbPlanningCollection()
+        {
+            if (_mongoCollection == null)
+            {
+                _mongoCollection = DataBase.GetCollection<PlanningModel>(_name);
+            }
+        }
+
+        public List<string> GetUniqueAccountsForLastDays(int days)
+        {
+            DateTime startDate = DateTime.Now.AddDays(-1 * days);
+            var planningRows = _mongoCollection
+                .Find(x => x.CreateDate > startDate)
+                .ToListAsync()
+                .Result;
+            var uniqueSessions = planningRows
+                .Select(x => x.Session)
+                .Distinct()
+                .ToList();
+            return uniqueSessions;
+        }
     }
 }
