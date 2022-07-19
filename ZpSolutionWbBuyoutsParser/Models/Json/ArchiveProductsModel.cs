53cd47748a39543ed39d7bca6779856728762ea6
Disaylt <disofban@gmail.com>
2022-07-19 11:47:03 +0300

    create archive products model

---
+++ ZpSolutionWbBuyoutsParser/Models/Json/ArchiveProductsModel.cs
@@ -0,0 +1,33 @@
+﻿using Global.ZennoLab.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZpSolutionWbBuyoutsParser.Models.Json
+{
+    internal class ArchiveProductsModel
+    {
+        [JsonProperty("code1S")]
+        public int ProductId { get; set; }
+
+        [JsonProperty("rId")]
+        public string OrderId { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("brand")]
+        public string Brand { get; set; }
+
+        [JsonProperty("price")]
+        public int Price { get; set; }
+
+        [JsonProperty("orderDate")]
+        public DateTime OrderDate { get; set; }
+
+        [JsonProperty("lastDate")]
+        public DateTime lastDate { get; set; }
+    }
+}
