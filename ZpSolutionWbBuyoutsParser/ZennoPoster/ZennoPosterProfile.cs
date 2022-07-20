922e7ea254e96646c101a2490dc2aadfd998e0f8
Disaylt <disofban@gmail.com>
2022-07-20 11:26:45 +0300

    create zennoposter profile

---
+++ ZpSolutionWbBuyoutsParser/ZennoPoster/ZennoPosterProfile.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZennoLab.InterfacesLibrary.ProjectModel.Collections;
+
+namespace ZpSolutionWbBuyoutsParser.ZennoPoster
+{
+    internal class ZennoPosterProfile
+    {
+        public string SessionName { get; private set; }
+
+        private readonly IProfile _profile;
+        private readonly string _pathToZpProfiles;
+
+        public ZennoPosterProfile(IProfile profile, string sessionName)
+        {
+            SessionName = sessionName;
+            _profile = profile;
+            WorkSettings workSettings = new WorkSettings();
+            _pathToZpProfiles = workSettings.GetSettings().PathToZpProfiles;
+        }
+        public void Load()
+        {
+            _profile.Load($"{_pathToZpProfiles}{SessionName}.zpprofile");
+        }
+
+        public void Save()
+        {
+            _profile.Save($"{_pathToZpProfiles}{SessionName}.zpprofile", 
+                saveLocalStorage: true, 
+                saveWebRtc: true, 
+                saveIndexedDb: true, 
+                saveSuperCookie: true);
+        }
+    }
+}
