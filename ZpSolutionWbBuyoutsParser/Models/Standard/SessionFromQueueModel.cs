6caf6ac66db037f714218b36e9774b7288f16ff7
Disaylt <disofban@gmail.com>
2022-07-25 11:56:43 +0300

    add session model for queue

---
+++ ZpSolutionWbBuyoutsParser/Models/Standard/SessionFromQueueModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZpSolutionWbBuyoutsParser.Models.Standard
+{
+    internal class SessionFromQueueModel
+    {
+        public string Name { get; set; }
+        public int CurrentAttempt { get; set; }
+    }
+}
