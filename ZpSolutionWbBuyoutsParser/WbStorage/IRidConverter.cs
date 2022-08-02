995859ceb7d58515ebf92343ce2b261637bec5a2
Disaylt <disofban@gmail.com>
2022-08-02 15:03:13 +0300

    create RidConverter

---
+++ ZpSolutionWbBuyoutsParser/WbStorage/IRidConverter.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZpSolutionWbBuyoutsParser.Models.Standard;
+
+namespace ZpSolutionWbBuyoutsParser.WbStorage
+{
+    internal interface IRidConverter<out T>
+    {
+        T ConvertToDifferentTypes();
+    }
+}
---
+++ ZpSolutionWbBuyoutsParser/WbStorage/StrOrIntRidConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZpSolutionWbBuyoutsParser.Models.Standard;
+
+namespace ZpSolutionWbBuyoutsParser.WbStorage
+{
+    internal class StrOrIntRidConverter : IRidConverter<RidTypesValueModel>
+    {
+        public RidTypesValueModel ConvertToDifferentTypes()
+        {
+            throw new NotImplementedException(); 
+        }
+    }
+}
